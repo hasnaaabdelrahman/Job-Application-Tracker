de6f9f4ac0a441f3b5e04f2bd788755e192896d7
Hasnaa <a02040456@gmail.com>
2026-03-31 03:16:55 +0200

    inject dtos && mapper into services

--- src/main/java/com/job/application/tracker/controller/CompanyController.java
+++ src/main/java/com/job/application/tracker/controller/CompanyController.java
@@ -1,5 +1,7 @@
 package com.job.application.tracker.controller;
 
+import com.job.application.tracker.dto.CompanyCreateDto;
+import com.job.application.tracker.dto.CompanyGetDto;
 import com.job.application.tracker.entity.Company;
 import com.job.application.tracker.service.CompanyServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +27,8 @@ public class CompanyController {
         return ResponseEntity.ok(companies);
     }
     @PostMapping("/add")
-    public ResponseEntity<Company> add(@RequestBody Company company) {
-        final Company addedCompany = companyServices.add(company);
+    public ResponseEntity<CompanyGetDto> add(@RequestBody CompanyCreateDto dto) {
+        final CompanyGetDto addedCompany = companyServices.add(dto);
         return ResponseEntity.ok(addedCompany);
     }
 }
--- src/main/java/com/job/application/tracker/service/CompanyServices.java
+++ src/main/java/com/job/application/tracker/service/CompanyServices.java
@@ -1,6 +1,9 @@
 package com.job.application.tracker.service;
 
+import com.job.application.tracker.dto.CompanyCreateDto;
+import com.job.application.tracker.dto.CompanyGetDto;
 import com.job.application.tracker.entity.Company;
+import com.job.application.tracker.mapper.CompanyMapper;
 import com.job.application.tracker.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +15,10 @@ public class CompanyServices {
     @Autowired
     CompanyRepository companyRepository;
 
-    public Company add(Company company) {
-        return companyRepository.save(company);
+    public CompanyGetDto add(CompanyCreateDto dto) {
+        Company company = CompanyMapper.toEntity(dto);
+         companyRepository.save(company);
+         return CompanyMapper.toDto(company);
     }
     public List<Company> getAll() {
         return companyRepository.findAll();
