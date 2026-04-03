770a7f9e57f70d3f4dda520a484cadf510659aa8
Hasnaa <a02040456@gmail.com>
2026-04-03 06:24:23 +0200

    add update && delete && get by id for company

--- src/main/java/com/job/application/tracker/controller/CompanyController.java
+++ src/main/java/com/job/application/tracker/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.job.application.tracker.controller;
 
 import com.job.application.tracker.dto.CompanyCreateDto;
 import com.job.application.tracker.dto.CompanyGetDto;
+import com.job.application.tracker.dto.CompanyUpdateDto;
 import com.job.application.tracker.entity.Company;
 import com.job.application.tracker.service.CompanyServices;
 import jakarta.validation.Valid;
@@ -27,9 +28,28 @@ public class CompanyController {
         final List<CompanyGetDto> companies = companyServices.getAll();
         return ResponseEntity.ok(companies);
     }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<CompanyGetDto> get(@PathVariable("id") Integer id) {
+        final CompanyGetDto company = companyServices.get(id);
+        return ResponseEntity.ok(company);
+    }
     @PostMapping("/add")
     public ResponseEntity<CompanyGetDto> add(@Valid @RequestBody CompanyCreateDto dto) {
         final CompanyGetDto addedCompany = companyServices.add(dto);
         return ResponseEntity.ok(addedCompany);
     }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CompanyGetDto> update(@PathVariable("id") Integer id , @RequestBody CompanyUpdateDto dto) {
+        final CompanyGetDto company = companyServices.update(id , dto);
+        return ResponseEntity.ok(company);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Company> delete(@PathVariable("id") Integer id) {
+        companyServices.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/job/application/tracker/dto/CompanyUpdateDto.java
+++ src/main/java/com/job/application/tracker/dto/CompanyUpdateDto.java
@@ -1,9 +1,13 @@
 package com.job.application.tracker.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyUpdateDto {
     @NotBlank
     private String name;
