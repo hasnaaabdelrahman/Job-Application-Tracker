850cdb35efe95249a73d3773b3059e777ef6ce42
Hasnaa <a02040456@gmail.com>
2026-04-06 17:35:38 +0200

    implement unit tests for delete

--- src/test/java/com/job/application/tracker/service/JobServiceTest.java
+++ src/test/java/com/job/application/tracker/service/JobServiceTest.java
@@ -1,4 +1,35 @@
 package com.job.application.tracker.service;
 
+import com.job.application.tracker.exceptions.ResourceNotFoundException;
+import com.job.application.tracker.repository.JobRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class JobServiceTest {
+    @Mock
+    JobRepository jobRepository;
+    @InjectMocks
+    JobService jobService;
+
+    @Test
+    void deleteJob_shouldThrowException_whenJobIsNotFound() {
+        when(jobRepository.existsById(999)).thenReturn(false);
+        assertThrows(ResourceNotFoundException.class , ()->
+                jobService.delete(999));
+    }
+
+    @Test
+    void deleteJob_shouldCallDelete_whenJobIsFound() {
+        when(jobRepository.existsById(1)).thenReturn(true);
+        jobService.delete(1);
+        verify(jobRepository).deleteById(1);
+    }
 }
