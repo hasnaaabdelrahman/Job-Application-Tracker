a615b52331966e0a8ba38247b3ab3e318fb7bbb3
Hasnaa <a02040456@gmail.com>
2026-05-31 19:31:39 +0300

    add find by token

--- src/main/java/com/job/application/tracker/repository/RefreshTokenRepository.java
+++ src/main/java/com/job/application/tracker/repository/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.job.application.tracker.repository;
 import com.job.application.tracker.model.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken , Integer> {
+    Optional<RefreshToken> findByToken(String token);
 }
