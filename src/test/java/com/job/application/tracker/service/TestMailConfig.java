2e0eca65a4014fe71440a2cd3d1a8e5166c2dfd6
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-08-24 22:08:42 +0300

    fix email temp

--- src/test/java/com/job/application/tracker/service/TestMailConfig.java
+++ src/test/java/com/job/application/tracker/service/TestMailConfig.java
@@ -4,8 +4,15 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.TestPropertySource;
 
 @TestConfiguration
+@TestPropertySource(properties = {
+        "spring.mail.username=test@example.com",
+        "spring.mail.password=testpassword",
+        "spring.mail.host=smtp.example.com",
+        "spring.mail.port=587"
+})
 public class TestMailConfig {
 
     @Bean
