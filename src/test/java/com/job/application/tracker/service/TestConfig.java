abfdcb279318690fa71c895dbdfb3b009e9a00cc
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-08-24 22:19:32 +0300

    fix: tests

--- src/test/java/com/job/application/tracker/service/TestConfig.java
+++ src/test/java/com/job/application/tracker/service/TestConfig.java
@@ -3,18 +3,16 @@ package com.job.application.tracker.service;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.test.context.TestPropertySource;
+import com.job.application.tracker.service.implementation.EmailService;
 
 import static org.mockito.Mockito.mock;
 
 @TestConfiguration
-public class TestMailConfig {
+public class TestConfig {
 
     @Bean
     @Primary
-    public JavaMailSender javaMailSender() {
-        return mock(JavaMailSender.class);
+    public EmailService emailService() {
+        return mock(EmailService.class);
     }
 }
