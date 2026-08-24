60a9f110cf77cbac34738db4aecd384b89948800
hasnaa <hasnaaabdelrahman60@gmail.com>
2026-08-24 21:56:23 +0300

    add env variables

--- src/test/java/com/job/application/tracker/ApplicationTests.java
+++ src/test/java/com/job/application/tracker/ApplicationTests.java
@@ -1,9 +1,12 @@
 package com.job.application.tracker;
 
+import com.job.application.tracker.service.TestMailConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestMailConfig.class)
 class ApplicationTests {
 
 	@Test
